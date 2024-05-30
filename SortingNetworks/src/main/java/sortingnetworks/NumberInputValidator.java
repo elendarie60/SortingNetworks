84c1a5a74e8d1971f104066b7287cd12bbdc9d67
LiviaL31 <59737886+LiviaL31@users.noreply.github.com>
2024-05-30 14:36:45 +0300

    Update NumberInputValidator.java

--- SortingNetworks/src/main/java/sortingnetworks/NumberInputValidator.java
+++ SortingNetworks/src/main/java/sortingnetworks/NumberInputValidator.java
@@ -13,6 +13,6 @@ public class NumberInputValidator {
                 return false;
             }
         }
-        return true;
+        return input.matches("(-?\\\\d+,\\\\s*)*-?\\\\d+");
     }
 }
