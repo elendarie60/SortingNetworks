455b7d5a7dbb3f2ce1a7d4d341f758c3c0e076a7
elendarie60 <79052848+elendarie60@users.noreply.github.com>
2024-06-01 12:32:00 -0800

    Update Comparator.java

--- SortingNetworks/src/main/java/sortingnetworks/Comparator.java
+++ SortingNetworks/src/main/java/sortingnetworks/Comparator.java
@@ -1,11 +1,44 @@
 package sortingnetworks;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
 public class Comparator {
-    public static void compareAndSwap(int[] array, int i, int j) {
-        if (array[i] > array[j]) {
-            int temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+
+    private final int from;
+    private final int to;
+    private Line line;
+
+
+    public Comparator(int from, int to, double yOffset, double yInterval, double xPosition) {
+        this.from = from;
+        this.to = to;
+        this.line = new Line(xPosition, yOffset + (from * yInterval), xPosition, yOffset + (to * yInterval));
+        this.line.setStroke(Color.RED);
+    }
+
+    public Comparator(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public void compareAndSwap(int[] array) {
+        if (array[from] > array[to]) {
+            int temp = array[from];
+            array[from] = array[to];
+            array[to] = temp;
         }
     }
 }
