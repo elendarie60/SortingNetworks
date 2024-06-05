197c1acb02d3314f2f2df6664c08b59997746133
elendarie60 <79052848+elendarie60@users.noreply.github.com>
2024-06-05 00:11:57 -0800

    Update Comparator.java

--- SortingNetworks/src/main/java/sortingnetworks/Comparator.java
+++ SortingNetworks/src/main/java/sortingnetworks/Comparator.java
@@ -4,41 +4,33 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class Comparator {
-
-    private final int from;
-    private final int to;
+    private final int i;
+    private final int j;
+    private final double yOffset;
+    private final double yInterval;
+    private final double xPosition;
+    private final int dir;
     private Line line;
 
-
-    public Comparator(int from, int to, double yOffset, double yInterval, double xPosition) {
-        this.from = from;
-        this.to = to;
-        this.line = new Line(xPosition, yOffset + (from * yInterval), xPosition, yOffset + (to * yInterval));
+    public Comparator(int i, int j, double yOffset, double yInterval, double xPosition, int dir, int arrayLength) {
+        this.i = i; this.j = j;
+        this.yOffset = yOffset;
+        this.yInterval = yInterval;
+        this.xPosition = xPosition;
+        this.dir = dir;
+        this.line = new Line(xPosition, yOffset + (this.i * yInterval), xPosition, yOffset + (this.j * yInterval));
         this.line.setStroke(Color.RED);
     }
 
-    public Comparator(int from, int to) {
-        this.from = from;
-        this.to = to;
-    }
-
-    public int getFrom() {
-        return from;
-    }
-
-    public int getTo() {
-        return to;
-    }
-
-    public Line getLine() {
-        return line;
-    }
-
     public void compareAndSwap(int[] array) {
-        if (array[from] > array[to]) {
-            int temp = array[from];
-            array[from] = array[to];
-            array[to] = temp;
+        if (i >= 0 && i < array.length && j >= 0 && j < array.length) {
+            if ((array[i] > array[j]) == (dir == 1)) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        } else {
+            System.err.println("index: i=" + i + ", j=" + j);
         }
     }
 }
