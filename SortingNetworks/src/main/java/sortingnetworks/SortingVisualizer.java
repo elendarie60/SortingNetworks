a3996a784442b2ab3d26941bca12ed2de23a9605
elendarie60 <79052848+elendarie60@users.noreply.github.com>
2024-06-05 00:17:12 -0800

    Update SortingVisualizer.java

--- SortingNetworks/src/main/java/sortingnetworks/SortingVisualizer.java
+++ SortingNetworks/src/main/java/sortingnetworks/SortingVisualizer.java
@@ -9,6 +9,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
+import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
+
 public class SortingVisualizer {
     private final int[] array;
     private final SortingNetworks sortingNetwork;
@@ -22,9 +25,9 @@ public class SortingVisualizer {
         this.array = array.clone();
         double yOffset = 30;
         double yInterval = 30;
-        double xPosition = 200; // Initial x position for comparators
-        this.sortingNetwork = new SortingNetworks(yOffset, yInterval, xPosition);
-         horizontalLines = new Line[array.length];
+        double xPosition = 200; // pozitia initiala a lui x pt comparatori
+        this.sortingNetwork = new SortingNetworks(yOffset, yInterval, xPosition, array.length);
+        this.horizontalLines = new Line[array.length];
         this.dottedLines = new Line[array.length];
     }
 
@@ -33,7 +36,7 @@ public class SortingVisualizer {
         pane.setPadding(new Insets(10));
 
         labels = new Label[array.length];
-        horizontalLines = new Line[array.length]; // Adaugăm inițializarea pentru linii orizontale
+        horizontalLines = new Line[array.length]; // linii orizontale
 
         double yOffset = 30;
         double yInterval = 30;
@@ -51,19 +54,13 @@ public class SortingVisualizer {
             labels[i].setPadding(new Insets(0, 10, 0, 10));
             pane.getChildren().add(labels[i]);
 
-            labels[i] = new Label(Integer.toString(array[i]));
-            labels[i].setLayoutX(width+5);
-            labels[i].setLayoutY(yOffset + (i * yInterval) - 10);
-            labels[i].setPadding(new Insets(0, 10, 0, 10));
-            pane.getChildren().add(labels[i]);
-
-              dottedLines[i] = new Line(40, yOffset + (i * yInterval)+dottedLineYOffset, 40, yOffset + (i * yInterval)+dottedLineYOffset);
-            dottedLines[i].setStroke(dottedLineColors[i%dottedLineColors.length]); // Schimbăm culoarea în roșu
-            dottedLines[i].getStrokeDashArray().addAll(5d, 5d); // Facem linia punctată
+            dottedLines[i] = new Line(40, yOffset + (i * yInterval) + dottedLineYOffset, 40, yOffset + (i * yInterval) + dottedLineYOffset);
+            dottedLines[i].setStroke(dottedLineColors[i % dottedLineColors.length]); // red change
+            dottedLines[i].getStrokeDashArray().addAll(5d, 5d); // linia punctata
             pane.getChildren().add(dottedLines[i]);
         }
 
-        for (Comparator comparator : sortingNetwork.getComparators()) {
+        for (SortingNetworks.Comparator comparator : sortingNetwork.getComparators()) {
             pane.getChildren().add(comparator.getLine());
         }
 
@@ -76,38 +73,43 @@ public class SortingVisualizer {
 
     private void runVisualization() {
         try {
-            for (Comparator comparator : sortingNetwork.getComparators()) {
+            for (SortingNetworks.Comparator comparator : sortingNetwork.getComparators()) {
                 Platform.runLater(() -> {
                     comparator.getLine().setStroke(Color.GREEN);
                 });
 
-                Thread.sleep(1000); 
+                Thread.sleep(1000);
 
                 comparator.compareAndSwap(array);
-                int index1 = comparator.getFrom();
-                int index2 = comparator.getTo();
+                int index1 = comparator.getI();
+                int index2 = comparator.getJ();
 
                 Platform.runLater(() -> {
                     labels[index1].setText(Integer.toString(array[index1]));
                     labels[index2].setText(Integer.toString(array[index2]));
                     double comparisonX = comparator.getLine().getStartX();
-                        dottedLines[index1].setEndX(comparisonX); // Actualizare capăt linie punctată
-                        dottedLines[index2].setEndX(comparisonX);
-                     double tempY = dottedLines[index1].getStartY();
-                        dottedLines[index1].setStartY(dottedLines[index2].getStartY());
-                        dottedLines[index2].setStartY(tempY);
-
+                    dottedLines[index1].setEndX(comparisonX); // linia punctata update
+                    dottedLines[index2].setEndX(comparisonX);
+                    double tempY = dottedLines[index1].getStartY();
+                    dottedLines[index1].setStartY(dottedLines[index2].getStartY());
+                    dottedLines[index2].setStartY(tempY);
                 });
 
-                Thread.sleep(1000); // Delay for visualization
+                Thread.sleep(1000); // Delay
 
                 Platform.runLater(() -> {
                     comparator.getLine().setStroke(Color.RED);
                 });
             }
+
+            // Vectorul final sortat
+            Platform.runLater(() -> {
+                for (int i = 0; i < array.length; i++) {
+                    labels[i].setText(Integer.toString(array[i]));
+                }
+            });
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
-
